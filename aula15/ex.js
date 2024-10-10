96b9b3a0ba49d470a3d986ea9060fe9b14fc8648
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-10 18:59:11 -0300

    .

--- aula15/ex.js
+++ aula15/ex.js
@@ -1,12 +1,11 @@
-const num = window.prompt('Digite um número')
+const num = Number(prompt('Digite um número'))
+const numeroTitulo = document.getElementById('titulo')
+const texto = document.getElementById('texto')
 
-    const numerioTitulo = document.getElementById = ('titulo')
-    const texto = document.getElementById = ('texto')
-    numeroTitulo.innerHTML += `Seu número é ${num}`
-
-    document.body.innerHTML += `Raiz quadrada: ${num ** 0.5} <br>`
-    document.body.innerHTML += `${num} é inteiro: ${Number.isNaN(num)} <br>`
-    document.body.innerHTML += `É NaN: ${Number.isInteger(num)} <br>`
-    document.body.innerHTML += `Arredondando para baixo: ${Math.floor(num)} <br>`
-    document.body.innerHTML += `Arredondando para cima: ${Math.ceil(num)} <br>`
-    document.body.innerHTML += `com duas casas decimais: ${num}`
+numeroTitulo.innerHTML = num
+texto.innerHTML += `<p> Raiz quadrada: ${num ** 0.5}. </p>`
+texto.innerHTML += `<p> ${num} é inteiro: ${Number.isInteger(num)}. </p>`
+texto.innerHTML += `<p> É NaN: ${Number.isNaN(num)}. </p>`
+texto.innerHTML += `<p> Arredondando para baixo: ${Math.floor(num)}. </p>`
+texto.innerHTML +=  `<p> Arredondando para cima: ${Math.ceil(num)}. </p>`
+texto.innerHTML += `<p> com duas casas decimais: ${num.toFixed(2)}. </p>`
