25066738fd790c591d25dc0d41267558fb448ab9
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-10 05:52:37 -0300

    .

---
+++ aula15/index.js
@@ -0,0 +1,10 @@
+
+
+// let num2 = Math.floor(num1) -> Arredonda para baixo
+// let num2 = Math.ceil(num1) -> Arredonda para cima
+// let num2 = Math.round(num1) -> Arredonda para o mais próximo
+// console.log(Math.min(1,2,3,4,5,6,-10,-11,-15,-100, 10000)) -> mostra o maior número da mostra o maior número da sequenciasequencia
+// console.log(Math.min(1,2,3,4,5,6,-10,-11,-15,-100, 10000)) -> mostra o menor número da sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+// console.log(Math.PI) -> valor de PI
+// console.log(Math.pow(2, 10)) -> potência
