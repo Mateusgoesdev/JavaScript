6eb5ca309f071e265e264643fb692000b3fc42a0
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-11 19:12:17 -0300

    .

---
+++ aula16/index.js
@@ -0,0 +1,30 @@
+//                 0        1       2
+const alunos = ['Maria', 'Joao', 'Jose'] // Luiza, Eduardo
+
+
+// console.log(alunos instanceof Array) // Para saber se é array
+// console.log(alunos instanceof Number)
+
+// console.log(alunos.slice(0, 3)) // fatia do índice 0 até o 3
+// console.log(alunos.slice(0, -1)) // fatia do índice 0 até o final e subtrai o numero negativo
+// console.log(alunos[50])
+
+// alunos.pop() // remove do final
+// alunos.shift() // remove do começo
+
+// alunos.unshift('Carlos') // adiciona valores no começo do array
+// alunos.unshift('Tatiana')
+
+// alunos.push('Felipe') // adiciona valores ao final do array
+// alunos.push('Pedro')
+
+// alunos[alunos.length] = 'Mariana' // adiciona valores ao array
+// alunos[alunos.length] = 'Fabio'
+// alunos[alunos.length] = 'Guilherme'
+
+// alunos[0] = 'Eduardo' // alterando os valores do array
+// alunos[3] = 'Luiza' // acrescentando valores no array
+
+//console.log(alunos)
+//console.log(alunos[0])
+//console.log(alunos[2])
