e0106ac946414e9282c0d3cb332a7d4e6d6c153e
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-15 09:28:39 -0300

    .

---
+++ aula19/script.js
@@ -0,0 +1,18 @@
+function meuEscopo() {
+    const form = document.querySelector('#form')
+    const resultado = document.querySelector('#resultado')
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault()
+            const nome = form.querySelector('#nome')
+            const sobrenome = form.querySelector('#sobrenome')
+            const peso = form.querySelector('#peso')
+            const altura = form.querySelector('#altura')
+
+            console.log(nome.value,sobrenome.value,peso.value,altura.value)
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+}
+meuEscopo()
