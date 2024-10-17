891cad3064e3a381de19edbe8988914c42348996
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-17 18:49:08 -0300

    .

--- aula19/script.js
+++ aula19/script.js
@@ -2,6 +2,8 @@ function meuEscopo() {
     const form = document.querySelector('#form')
     const resultado = document.querySelector('#resultado')
 
+    const pessoas = []
+
     function recebeEventoForm (evento) {
         evento.preventDefault()
             const nome = form.querySelector('#nome')
@@ -9,7 +11,16 @@ function meuEscopo() {
             const peso = form.querySelector('#peso')
             const altura = form.querySelector('#altura')
 
-            console.log(nome.value,sobrenome.value,peso.value,altura.value)
+            pessoas.push({
+                nome: nome.value,
+                sobrenome: sobrenome.value,
+                peso: peso.value,
+                altura: altura.value
+            })
+
+            console.log(pessoas)
+
+      resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}`
     }
 
     form.addEventListener('submit', recebeEventoForm)
