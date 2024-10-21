532fd74bcfad6ebd996eb582c4cdeb1d2104c9e1
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-21 14:40:11 -0300

    .

---
+++ aula24/index.js
@@ -0,0 +1,25 @@
+
+// if pode ser usado sozinho
+// sempre que utilizo a palavra else, preciso de um if antes
+// eu posso ter vários else ifs na checagem
+// do posso ter um else na checagem
+//podemos usar condições sem else if, utilizando apenas if e else
+ 
+ /*
+ const hora = 50
+if (hora < 12) {
+    console.log('Bom dia')
+ } else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde')
+ } else if (hora > 18 && hora < 24 ) {
+    console.log('Boa noite')
+ } else {
+    console.log('Digite um horário válido')
+ }*/
+
+const tenhoGrana = false
+if (tenhoGrana) {
+    console.log('vou sair de casa')
+} else {
+    console.log('vou ficar em casa')
+}
