57005ff639e22fd8b8438d9a294f1b2194f01665
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-21 14:12:22 -0300

    .

---
+++ aula22/inderx.js
@@ -0,0 +1,17 @@
+/*    Operadores lógicos
+  && -> AND -> E // Todas as expressões precisam ser verdadeiras para retornar TRUE
+  || -> OR -> OU
+/ ! -> NOT -> NÃO 
+*/
+
+// const expressaoAnd = true && true && true && true
+// const expressaoOr = true || false || true || true
+// const expressaoNot = !false
+// console.log(expressaoNot)
+
+/* const usuario = 'Mateus'
+const senha = '123456'
+
+const vaiLogar = usuario === 'Mateus' && senha === '123456'
+console.log(vaiLo gar)
+*/
---
+++ aula23/index.js
@@ -0,0 +1,13 @@
+/*
+&& = false && true -> false "o valor mesmo"
+|| = false || true -> true "vai retornar verdadeiro"
+false
+0
+'' "" ```
+null / undefined
+NaN
+*/
+const corUsuario = 'preto'
+const corPadrao = corUsuario || 'red'
+console.log(corPadrao)
+
