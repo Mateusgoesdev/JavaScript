a56f9db9a2d81cf5b3988f5ef65c74a81dab9a42
Mateus Goes <165453708+Mateusgoesdev@users.noreply.github.com>
2024-10-24 10:24:55 -0300

    .

--- aula25/index.js
+++ aula25/index.js
@@ -4,5 +4,5 @@ const numero = 10
 if (numero >=0 && numero <=5 ) {
     console.log('O número esta entre 0 e 5')
 } else {
-    console.log('O número NÃO esta entre 0 e 5')
+    console.log('O número NÃO esta entre 0 e 5')    
 }
